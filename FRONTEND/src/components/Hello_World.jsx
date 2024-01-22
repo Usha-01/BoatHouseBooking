888c021ca17736b7b9523254453745a06b5ece7a
Usha Kalathinathan <ushasasi7018@gmail.com>
2024-01-22 15:34:35 +0530

    FRONTEND
    Creation of Vite+React

---
+++ FRONTEND/src/components/Hello_World.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+function HelloWorld () {
+    
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        );
+    
+}
+
+export default HelloWorld;
